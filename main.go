c0b52e9279e3fae28329c41a581806e71d1f906e
Anne-Okingo <annekadiso@gmail.com>
2026-01-20 15:54:11 +0300

    feat: add contact form handlers

--- main.go
+++ main.go
@@ -4,12 +4,17 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"my-portfolio/Backend/handlers"
 )
 
 func main() {
 	// Serve all files in current directory
 	fs := http.FileServer(http.Dir("."))
 	http.Handle("/", fs)
+
+	// Register contact handler
+	http.HandleFunc("/contact", handlers.ContactHandler)
 
 	fmt.Println("Portfolio server running at http://localhost:3000")
 	fmt.Println("Your website is now live with full CSS styling!")
